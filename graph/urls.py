c79c7cabaf90acecd8ca924931b2b31fb1dd6db6
Dipesh Sinha <dipeshsinha.ds@gmail.com>
2019-03-24 01:05:27 +0530

    new

--- graph/urls.py
+++ graph/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from graph import views
 
 urlpatterns = [
-    path('line',views.line)
+    path('line',views.line),
+    path('bar',views.bar),
+    path('hist',views.hist),
+    path('pie',views.pie)
 ]
--- graph/views.py
+++ graph/views.py
@@ -38,3 +38,90 @@ def line(request):
         except:
             response = json.dumps([{'error': 'unable to create graph'}])
         return HttpResponse(response, content_type = 'text/json')
+@csrf_exempt
+def bar(request):
+    if request.method == 'POST':
+        try:
+            x = json.loads(request.body)
+            arrx = x['x']
+            arry = x['y']
+            if x['lcol'] == '':
+                col = 'green'
+            else:
+                col = x['lcol']
+            plt.clf()
+            plt.bar(arrx,arry, color=col, width=x['lwidth'])
+
+            if x['xlabel'] != '':
+                plt.xlabel(x['xlabel'])
+            if x['ylabel'] != '':
+                plt.ylabel(x['ylabel'])
+            if x['gname'] != '':
+                plt.title(x['gname'])
+
+            plt.savefig('plot.png', dpi=int(x['quality']))
+            with open('plot.png', 'rb') as imageFile:
+                data = base64.b64encode(imageFile.read()).decode()
+            response = json.dumps([{'image': data}])
+        except:
+            response = json.dumps([{'error': 'unable to create graph'}])
+        return HttpResponse(response, content_type = 'text/json')
+@csrf_exempt
+def hist(request):
+    if request.method == 'POST':
+        try:
+            x = json.loads(request.body)
+            arrx = x['x']
+            range = x['range']
+            if x['col'] == '':
+                col = 'green'
+            else:
+                col = x['col']
+            plt.clf()
+            if len(range) == 1:
+                plt.hist(arrx, rwidth=x['width'], color=col)
+            else :
+                plt.hist(arrx, bins=range, rwidth=x['width'], color=col)
+
+            if x['xlabel'] != '':
+                plt.xlabel(x['xlabel'])
+            if x['ylabel'] != '':
+                plt.ylabel(x['ylabel'])
+            if x['gname'] != '':
+                plt.title(x['gname'])
+
+            plt.savefig('plot.png', dpi=int(x['quality']))
+            with open('plot.png', 'rb') as imageFile:
+                data = base64.b64encode(imageFile.read()).decode()
+            response = json.dumps([{'image': data}])
+        except:
+            response = json.dumps([{'error': 'unable to create graph'}])
+        return HttpResponse(response, content_type = 'text/json')
+@csrf_exempt
+def pie(request):
+    if request.method == 'POST':
+        try:
+            x = json.loads(request.body)
+            arrx = x['x']
+            arry = x['y']
+            # if x['lcol'] == '':
+            #     col = 'green'
+            # else:
+            #     col = x['lcol']
+            plt.clf()
+            plt.pie(arrx, labels=arry, autopct='%0.1f%%', radius=1.2, explode=x['exp'])
+
+            # if x['xlabel'] != '':
+            #     plt.xlabel(x['xlabel'])
+            # if x['ylabel'] != '':
+            #     plt.ylabel(x['ylabel'])
+            # if x['gname'] != '':
+            #     plt.title(x['gname'])
+
+            plt.savefig('plot.png', dpi=int(x['quality']))
+            with open('plot.png', 'rb') as imageFile:
+                data = base64.b64encode(imageFile.read()).decode()
+            response = json.dumps([{'image': data}])
+        except:
+            response = json.dumps([{'error': 'unable to create graph'}])
+        return HttpResponse(response, content_type = 'text/json')
